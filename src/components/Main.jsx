041ccb353d2d08d1c0a733855ab22ce8f5918c9b
fluid-coder <tm.aashish422@gmail.com>
2024-06-07 22:40:44 +0545

    changes made

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 
 function Main() {
   return (
-    <div className='w-full h-screen' class="bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-violet-900 via-purple-950  to-black to-90%">
-      <div class="size-fit">
-{/* image or video */}
-<video src='\videos\yatri_video.mp4'></video>
-        </div>
-        <div className="text" class="text-blue-300">
+    <div className='w-full h-screen'>
+      <div className="text text-blue-300">
             Reinventing Urban Mobility
-        </div>
-        <div className="bike">
-{/* image or video */}
-<video src="/videos/yatri_video"></video>
-        </div>
+      </div>
+      <div className="size-fit">
+          {/* image or video */}
+          <video src='/videos/yatri_video.webm' autoplay loop ></video>
+        </div> 
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,26 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='nav'>
-        <div className='logo' class="bg-black">
+    <div className='nav flex justify-center w-full bg-pink-800'>
+        <div className='logo bg-black size-20'>
         <img src="/images/yatri_logo.png" alt="logo" />
         </div>
-        <div className='links' class="text-blue-300 space-x-5 pl-5 bg-black">
+        <div className='links text-blue-300 space-x-5 pl-5 bg-black'>
             {/* only this much is shown initially */}
             {/* other navbar options will be shown after clicking on hamburger menu */}
             {["MODELS", "ENERGY" , "TEST RIDE"].map((item, index)=>{
                return <a key={index} className='nav_link'>{item}</a>
             })}
         </div>
-        <div className='logo' class="bg-black">
+
+            <div className='hamburger_menu bg-blue-500'>
             {/* use hamburger menu */}
-            <div className='hamburger_menu color green'>
               <span>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>fh</div>
+                <div>dghd</div>
+                <div>dhdf</div>
               </span>
             </div>
-        {/* <img src="/images/" alt="hamburger menu" /> */}
-        </div>
     </div>
   )
 }
