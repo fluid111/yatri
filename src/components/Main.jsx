7b864efe7614443ee9fc398d2b95f7e0dee21091
subodhdhital <subodhdhital79@gmail.com>
2024-06-07 12:53:31 +0545

    gradient

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Main() {
   return (
-    <div className='w-full h-screen'>
-        <div className="text">
-            Reinventing Urban Mobility
-        </div>
-        <div className="bike">
+    <div className='w-full h-screen' class="bg-gradient-to-bl from-black to-100% via-violet-900 to-90%  to-purple-950 to-90%">
+      <div class="size-fit">
 {/* image or video */}
-<video src='/videos/yatri_video'></video>
+<video src='\videos\yatri_video.mp4'></video>
+        </div>
+        <div className="text" class="text-blue-300">
+            Reinventing Urban Mobility
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className=''>
-        <div className='logo'>
+    <div className='nav'>
+        <div className='logo' class="bg-black">
         <img src="/images/yatri_logo.png" alt="logo" />
         </div>
-        <div className='links'>
+        <div className='links' class="text-blue-300 space-x-5 pl-5 bg-black">
             {/* only this much is shown initially */}
             {/* other navbar options will be shown after clicking on hamburger menu */}
             {["MODELS", "ENERGY" , "TEST RIDE"].map((item, index)=>{
                return <a key={index} className='nav_link'>{item}</a>
             })}
         </div>
-        <div className='logo'>
+        <div className='logo' class="bg-black">
             {/* use hamburger menu */}
         <img src="/images/" alt="hamburger menu" />
         </div>
