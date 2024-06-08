42a1f56df2bae831cd8706475ff6b68e3aa21a22
subodhdhital <subodhdhital79@gmail.com>
2024-06-08 09:39:29 +0545

    nav edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='nav flex w-full bg-pink-800'>
-        <div className='logo bg-black justify-start'>
+    <div className='p-[5px] bg-black'>
+    <div className='nav flex w-full bg-black justify-between h-[70px]'>
+        <div className='logo bg-black size-[70px] place-content-center'>
         <img src="/images/yatri_logo.png" alt="logo" />
         </div>
-        <div className='links text-blue-300 space-x-5 p-5 bg-black size-full text-right'>
+        <div className='header hidden md:flex'>
+        <div className='links text-blue-300 space-x-[45px] pt-[20px] pr-[20px] bg-black size-full text-right text-lg'>
             {/* only this much is shown initially */}
             {/* other navbar options will be shown after clicking on hamburger menu */}
             {["MODELS", "ENERGY" , "TEST RIDE"].map((item, index)=>{
                return <a key={index} className='nav_link'>{item}</a>
             })}
-        </div>
-            <div className='hamburger_menu bg-blue-500 size-16 text-xs'>
+            </div>
+            <div className='hamburger_menu bg-blue-500 size-[70px] text-xs'>
             {/* use hamburger menu */}
               <span>
                 <div>fh</div>
@@ -21,6 +23,8 @@ function Navbar() {
                 <div>dhdf</div>
               </span>
             </div>
+            </div>
+    </div>
     </div>
   )
 }
