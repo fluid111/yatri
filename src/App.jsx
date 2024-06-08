149b76eab3312730543bf75849bc98817573b843
fluid-coder <tm.aashish422@gmail.com>
2024-06-08 19:58:18 +0545

    text problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import { useState } from 'react';
 
 function App() {
+  const[toggle, setToggle] = useState(false);
+
   return (
     <div className='w-full h-screen bg-zinc-900 text-white'>
-      <Navbar/>
-      <Main/>
+      <Navbar toggle={toggle} setToggle={setToggle} />
+      <Main toggle={toggle} />
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,7 @@
-
 import { useEffect, useRef } from "react";
 import React from 'react';
 
-function Main() {
+function Main({toggle, setToggle}) {
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -15,11 +14,21 @@ function Main() {
     <div className=' bg-background'>
       <div className="object-fit-cover">
         {/* image or video */}
-        <video ref={videoRef} src='/videos/yatri_video.webm' muted loop autoplay className="w-full h-full object-cover"></video>
+        <video ref={videoRef}
+               src='/videos/yatri_video.webm'
+               muted
+               loop
+               autoPlay
+               className="w-full h-full object-cover"></video>
       </div> 
-      <div className={"text absolute text-white top-0 left-0 pl-[140px] pt-[130px] text-5xl "}>
+      <div className={`text absolute text-white top-0 left-0 pl-[140px] pt-[130px] text-5xl
+                       ${toggle ? 'hidden md:hidden' : ''}`
+                       }>
         <div className='pb-[18px] font-bold font-heebo'>Reinventing</div>
-        <div className='pt-[5px] space-x-[13px] object-fit-cover font-semibold flex font-heebo'>Urban <div className="pl-[25px] text-blue-300">Mobility</div></div>
+        <div className='pt-[5px] space-x-[13px] object-fit-cover font-semibold flex font-heebo'>Urban
+            <div className="pl-[25px] text-blue-300">Mobility
+            </div>
+          </div>
       </div>
     </div>
   );
