f35839eed1485b1d59b8d6d8d2bca472bbd9b5d2
fluid-coder <tm.aashish422@gmail.com>
2024-06-12 20:15:12 +0545

    footer added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function footer() {
+function Footer() {
   return (
     <div>
         <div className="footer">
             <div className="footer_image"></div>
             <div className="footer_text">
-                <div className="footer_text1"></div>
-                <div className="footer_text2"></div>
+                <div className="footer_text1">footer 1</div>
+                <div className="footer_text2">footer 2</div>
             </div>
             <div className="map"></div>
             <div className="contact_info"></div>
@@ -16,4 +16,4 @@ function footer() {
   )
 }
 
-export default footer
+export default Footer
